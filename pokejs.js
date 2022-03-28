757fd8d94cb1c5c048d46b27b73cb6e194830a0d
Karime Kirey <81454740+karimekirey@users.noreply.github.com>
2022-03-27 20:33:45 -0600

    Agregué archivos

---
+++ pokejs.js
@@ -0,0 +1,31 @@
+
+/*función para obtener los datos de la API*/
+const fetchPokemon = () => {
+    const pokeNameInput = document.getElementById("pokeName");
+    let pokeName = pokeNameInput.value;
+    pokeName = pokeName.toLowerCase();
+    /*pokeNameMayus = pokeName.toUpperCase();*/
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
+    fetch(url).then((res) => {
+        if (res.status != "200") {
+            console.log(res);
+            pokeImage("./signo.png");
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+            let pokeImg = data.sprites.front_default;
+            pokeImage(pokeImg);
+            console.log(pokeImg);
+        }
+    });
+}
+
+/ */
+const pokeImage = (url) => {
+    const pokePhoto = document.getElementById("pokeImg");
+    pokePhoto.src = url;
+}
